d47ada007150b0dae6e29b3767cb1cf711ceac43
suphattra245 <jonesuphattra3@gmail.com>
2025-03-13 21:23:33 +0700

    second commit

--- src/components/AI.tsx
+++ src/components/AI.tsx
@@ -9,7 +9,7 @@ export default function AI() {
           <img
             className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
             alt="hero"
-            src="images/p22.jpg"
+            src="images/kk12.jpg"
           />
           <div className=" lg:w-2/3 w-full">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 text-center">
